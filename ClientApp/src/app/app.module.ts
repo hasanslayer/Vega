f4e3a4735a370e080e9d07f915fe3ecefcc16b21
hasanslayer <hasan-dr2010@hotmail.com>
2018-06-30 22:28:02 +0200

    Populating Drop-down Lists

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -24,10 +25,11 @@ import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    HttpModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      {path:'vehicles/new',component:VehicleFormComponent},
+      { path: 'vehicles/new', component: VehicleFormComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
