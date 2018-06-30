01399bdd1c13000ab9ac9d4c09d3a5932308ad29
hasanslayer <hasan-dr2010@hotmail.com>
2018-06-30 16:49:07 +0200

    Applying Constraints

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Vega.Models
 {
+    [Table("Models")] // to make database name "Models" NOT "Model"
     public class Model
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public Make Make { get; set; }
         public int MakeId { get; set; }
-        
+
     }
 }
