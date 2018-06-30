0b0223cb95b24557eaa54ecf553b9bf1b93e1955
hasanslayer <hasan-dr2010@hotmail.com>
2018-06-30 15:28:39 +0200

    Edit in Domain Model

--- Models/Model.cs
+++ Models/Model.cs
@@ -4,6 +4,8 @@ namespace Vega.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
         
     }
 }
