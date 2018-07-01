406cb30d0be9f2f0dfa3296de0c5d119ae5f5ac4
hasanslayer <hasan-dr2010@hotmail.com>
2018-07-01 23:19:35 +0200

    Building an API to Update Objects

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -25,11 +25,28 @@ namespace Vega.Controllers
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-                
+
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
 
             context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody]VehicleResource vehicleResource)//  FromBody : the data of object [complex object] in the body of the request
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.FindAsync(id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);// (from,to)
+            vehicle.LastUpdate = DateTime.Now;
+
+        
             await context.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
