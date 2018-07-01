58b28ecbf1274c7af5f480ed1a695c47cbc9208c
hasanslayer <hasan-dr2010@hotmail.com>
2018-07-01 18:56:59 +0200

    Creating an API Resource

---
+++ Controllers/Resources/VehicleResource.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Vega.Models;
+
+namespace Vega.Controllers.Resources
+{
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+
+        public bool IsRegistered { get; set; }
+        public ContactResource Contact { get; set; }
+
+
+        public ICollection<int> Features { get; set; } //int : beacuse we want to send just int [ id of feature]
+
+        public VehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
