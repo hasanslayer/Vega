149b824a1c7e20fa4ebb7574cd343dc1595a5dfe
hasanslayer <hasan-dr2010@hotmail.com>
2018-07-02 17:43:26 +0200

    Refactoring Using LINQ

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -29,18 +29,15 @@ namespace Vega.Mapping
                 .AfterMap((vr, v) =>
                 {
                     // remove unselected features
-                    var removedFeatures = new List<VehicleFeature>(); //this list because we can't remove Features While iteration {Exception}
-                    foreach (var f in v.Features)
-                        if (!vr.Features.Contains(f.FeatureId))
-                            removedFeatures.Add(f);
+                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
                     foreach (var f in removedFeatures)
                         v.Features.Remove(f);
 
-                    // Add new features 
-                    foreach (var id in vr.Features)
-                        if (!v.Features.Any(f => f.FeatureId == id))
-                            v.Features.Add(new VehicleFeature { FeatureId = id });
 
+                    // Add new features 
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature { FeatureId = id });
+                    foreach (var f in addedFeatures)
+                        v.Features.Add(f);
 
                 }); //AfterMap(source obj,target obj)
 
